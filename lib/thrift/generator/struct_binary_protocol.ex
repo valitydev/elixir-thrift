f188417f73f0794c374a579d3a9d3732e850daae
Aleksey Kashapov <nanodirijabl@gmail.com>
2024-05-07 14:40:07 +0300

    Adds note about escaped names list

--- lib/thrift/generator/struct_binary_protocol.ex
+++ lib/thrift/generator/struct_binary_protocol.ex
@@ -1244,6 +1244,7 @@ defmodule Thrift.Generator.StructBinaryProtocol do
   defp type_id(%Union{}, _), do: Type.struct()
   defp type_id(type, _), do: Type.of(type)
 
+  # NOTE As in https://github.com/elixir-lang/elixir/blob/b10df9cd539af980851b7878f8a86d7bbdc4c0bd/lib/elixir/src/elixir_errors.erl#L336-L356
   defp escaped_var(name)
        when name in ~w"not and or when after catch end fn else rescue true false nil in"a,
        do: Macro.var(:"#{name}_", nil)
